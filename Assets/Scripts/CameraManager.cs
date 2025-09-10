a9a2107da2b468e78f3a49cc225a3bc18dab54f2
Luna 3 <semideia723@gmail.com>
2025-09-10 08:32:11 -0300

    Swipe to change screen added

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    [SerializeField] private Transform[] cameraPoints;
+    [SerializeField] private float moveSpeed = 5f;
+    private int currentPointIndex = 2;
+    private Rigidbody2D rb;
+    private Vector2 startTouchPosition, endTouchPosition;
+    [SerializeField] private float swipeThreshold;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.MovePosition(cameraPoints[2].position);
+    }
+
+    void Update()
+    {
+        MoveCamera();
+    }
+
+    private void MoveCamera()
+    {
+        MoveCameraLeft();
+        MoveCameraRight();
+    }
+    public void MoveCameraLeft()
+    {
+        Swipe();
+        if (Input.GetKeyDown(KeyCode.LeftArrow) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended && startTouchPosition.x < endTouchPosition.x - swipeThreshold)
+        {
+            if (currentPointIndex > 0) currentPointIndex -= 1;
+            StopAllCoroutines();
+            StartCoroutine(MoveToPosition(gameObject, cameraPoints[currentPointIndex].position, moveSpeed));
+        }
+    }
+
+    public void MoveCameraRight()
+    {
+        Swipe();
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended && startTouchPosition.x > endTouchPosition.x + swipeThreshold)
+        {
+            if (currentPointIndex < 4) currentPointIndex += 1;
+            StopAllCoroutines();
+            StartCoroutine(MoveToPosition(gameObject, cameraPoints[currentPointIndex].position, moveSpeed));
+        }
+    }
+
+    private void Swipe()
+    {
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
+        {
+            startTouchPosition = Input.GetTouch(0).position;
+        }
+        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Ended)
+        {
+            endTouchPosition = Input.GetTouch(0).position;
+        }
+    }
+
+    private IEnumerator MoveToPosition(GameObject go, Vector2 movePosition, float s)
+    {
+        while ((Vector2)go.transform.position != movePosition)
+        {
+            go.transform.position = Vector2.MoveTowards(go.transform.position, movePosition, s * Time.deltaTime);
+            startTouchPosition = Vector2.zero;
+            endTouchPosition = Vector2.zero;
+            yield return null;
+        }
+
+    }
+
+    
+}
